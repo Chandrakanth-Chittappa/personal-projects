fb683c0eb01f9565d4089302c73adea57803b44b
Chandrakanth <apple@Apples-MacBook-Pro.local>
2019-11-18 13:34:35 -0500

    first commit

---
+++ forecastapplication/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Car from './App.js';
+
+const myfirstelement = <h1>Hello React!</h1>
+
+ReactDOM.render(<Car />, document.getElementById('root'));
