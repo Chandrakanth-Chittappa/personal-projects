cc6e5986410eadd7678c6b04d104a0f62f426d07
Chandrakanth Chittappa <chandrakanthyadav50.cy@gmail.com>
2019-11-18 14:06:09 -0500

    first commit to weather application

--- forecastapplication/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Car from './App.js';
-
-const myfirstelement = <h1>Hello React!</h1>
-
-ReactDOM.render(<Car />, document.getElementById('root'));
